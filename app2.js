305b72722201e2d5c952e912379e9a01788688c1
sergei <sverkeenko1303@gmail.com>
2022-11-02 21:44:17 +0300

    calc

--- app2.js
+++ app2.js
@@ -3,7 +3,25 @@ const result = document.querySelector('#result')
 calc.addEventListener('click', function (event) {
     if (!event.target.classList.contains('calc-btn')) return;
     const value = event.target.innerText;
+
     switch (value) {
+
+        case 'cos':
+            result.innerText = `${Math.cos(result.innerText)}`
+            break;
+
+        case '♥':
+            result.innerText = 'Калькулятор признается тебе в любви'
+            break;
+
+        case 'sin':
+            result.innerText = `${Math.sin(result.innerText)}`
+            break;
+
+        case 'корень':
+            result.innerText = `${Math.sqrt(result.innerText)}`
+            break;
+
         case 'C':
             result.innerText = ''
             break;
